6d94237feb035eb1b1ee3bdea8f45beeacc49050
João Paulo Fricks <jpfricks@gmail.com>
2024-05-22 14:22:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMembers() {
+  var members = [
+    { name: 'John', skills: ['JavaScript', 'React', 'Node'] },
+    { name: 'Jane', skills: ['Kotlin', 'Swift', 'Java'] },
+    { name: 'Jack', skills: ['Golang', 'Docker', 'Kubernetes'] },
+  ];
+
+  return members;
+}
